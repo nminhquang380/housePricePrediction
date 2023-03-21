55e8d33d00448346ed884d400500dfc6bd9e7794
Minh Quang Nguyễn <nminhquang380@gmail.com>
2023-03-21 11:31:41 +1100

    [ADD] Complete Visualizing and Exploring, wrap data python file into a class

--- source/data.py
+++ source/data.py
@@ -9,21 +9,25 @@ import tarfile
 import urllib.request
 from sklearn.model_selection import train_test_split
 
+class Data:
 # Step to get data:
 # Create a path -> Check existance 
-# -> If not exist -> Make path, download tarfile -> Extract File 
-def load_housing_data():
-    data_path = Path('data/housing.tgz')
-    if not data_path.is_file():
-        Path('data').mkdir(parents=True, exist_ok=True)
-        url = "https://github.com/ageron/data/raw/main/housing.tgz"
-        urllib.request.urlretrieve(url, data_path)
-        with tarfile.open(data_path) as housing_data:
-            housing_data.extractall(path="data")
-    return pd.read_csv(Path("data/housing/housing.csv"))
+# -> If not exist -> Make path, download tarfile -> Extract File
+    def __init__(self) -> None:
+        self.housing = None
+        
+    def load_housing_data(self):
+        data_path = Path('data/housing.tgz')
+        if not data_path.is_file():
+            Path('data').mkdir(parents=True, exist_ok=True)
+            url = "https://github.com/ageron/data/raw/main/housing.tgz"
+            urllib.request.urlretrieve(url, data_path)
+            with tarfile.open(data_path) as housing_data:
+                housing_data.extractall(path="data")
+        return pd.read_csv(Path("data/housing/housing.csv"))
 
-housing = load_housing_data()
-
-# Split Test set
-def train_test_set(data, test_ratio):
-    return train_test_split(data, test_size=test_ratio, random_state=42)
+    
+    # Split Test set
+    def train_test_set(self):
+        self.housing = pd.read_csv(Path("data/housing/housing.csv"))
+        return train_test_split(self.housing, test_size=0.2, random_state=42)
---
+++ source/init.py
@@ -0,0 +1,1 @@
+from source.data import Data
